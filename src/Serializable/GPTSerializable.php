d7bf91a88334289db0486e4cda5d19fff1b917de
Lukas Mateffy <github@mateffy.me>
2022-12-13 18:56:08 +0100

    Removed internal serializable data structures

--- src/Serializable/GPTSerializable.php
+++ src/Serializable/GPTSerializable.php
@@ -4,6 +4,12 @@ namespace Capevace\GPT\Serializable;
 
 use JsonSerializable;
 
+/**
+ * This class is a helper for serializing objects to JSON.
+ *
+ * This is useful if you're trying to embed a JSON object into your prompt,
+ * which should follow a specific format.
+ */
 abstract class GPTSerializable implements JsonSerializable
 {
     public function jsonSerialize(): array
